b04cff1d7faccfe1a104030ca9836df1e497aaa5
Yali <yali.soltz11@gmail.com>
2026-04-27 21:41:34 +0300

    Remove loading spinner - ISR makes pages fast enough

--- app/(public)/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="min-h-[80vh] flex items-center justify-center">
-      <div className="flex flex-col items-center gap-4">
-        <div className="w-8 h-8 border-2 border-[#5C3D2E]/20 border-t-[#5C3D2E] rounded-full animate-spin" />
-      </div>
-    </div>
-  )
-}
